13555ca63c99e694515eafc3efdba9162639374b
Konstantin <kazachenko.1998k@gmail.com>
2018-05-26 16:31:08 +0300

    add new command+exception

--- main.c
+++ main.c
@@ -5,10 +5,11 @@
 #include <stdbool.h>
 
 
-double Ac = 0, R = 0;
-int x;
+double Ac = 0;
+int x, R = 0;
 char *com;
 char *command;
+double *registerData;
 FILE *fp, *out;
 int countSteps = 0, key, i;
 int curi = 0;
@@ -21,6 +22,8 @@ int pars(FILE *fp, int debag);
 int main() {
     com = malloc(sizeof(char) * 10);
     command = malloc(sizeof(char) * 10);
+    registerData = malloc(sizeof(double) * 1024);
+    memset(registerData, 0, sizeof(registerData));
     fp = fopen("input.txt", "r");
     out = fopen("output.txt", "w");
     stop:;
@@ -28,17 +31,18 @@ int main() {
            " fr       Full running \n"
            " rfors    Running count of steps \n"
            " sbs      Run code step by step \n"
-           " sbs -deb Run code step by step with printing results \n");
+           " sbs -deb Run code step by step with printing results \n"
+           " exit     exit from program");
     gets(answer);
     if (strcmp(answer, "fr") == 0) {//полное выполнение кода
         while (!feof(fp)) {
-            if (pars(fp, 0) == 666) goto fail;
+            if (pars(fp, 0) != 0) goto fail;
         }
     } else if (strcmp(answer, "rfors") == 0) { //выполнение определенного числа шагов
         printf("Input count of steps \n");
         scanf("%d", &countSteps);
         while (countSteps > 0) {
-            if (pars(fp, 0) == 666) goto fail;
+            if (pars(fp, 0) != 0) goto fail;
             countSteps--;
         }
     } else if (strcmp(answer, "sbs") == 0) { //выполнить код пошагово
@@ -47,13 +51,13 @@ int main() {
             key = getch();
             switch (key) {
                 case 32:// Space- следующая команда
-                    if (pars(fp, 0) == 666) goto fail;
+                    if (pars(fp, 0) != 0) goto fail;
                     break;
                 case 27://Esc - прекратить выполнение программы
                     break;//завершаем работу программы
                 default:
                     while (!feof(fp)) {
-                        if (pars(fp, 0) == 666) goto fail;
+                        if (pars(fp, 0) != 0) goto fail;
                     }
                     break;
             }
@@ -66,21 +70,22 @@ int main() {
             switch (key) {
                 case 32:
                     // Space- следующая команда
-                    if (pars(fp, 1) == 666) goto fail;
+                    if (pars(fp, 1) != 0) goto fail;
                     printf("Ac: %2.3f R: %2.3f \n", Ac, R);
                     break;
                 case 27://Esc - прекратить выполнение программы
                     break;//завершаем работу программы
                 default:
                     while (!feof(fp)) {
-                        if (pars(fp, 0) == 666) goto fail;
+                        if (pars(fp, 0) != 0) goto fail;
                     }
                     break;
             }
             if (key == 27) break;
         }
-    } else {
-        printf("not found command");
+    } else if (strcmp(answer, "exit") == 0) return 0;
+    else {
+        printf("Not found command\n");
         goto stop;
     }
     fclose(fp);
@@ -102,6 +107,8 @@ int pars(FILE *fp, int debag) {
     fgets(command, 100, fp);//читаем очередную строку
     if (debag) {
         printf(command);
+        for (i = 0; i<1024; i++)
+        printf("%2.3f; ", registerData[i]);
     }
     curi = 0;
     for (int i = 0; i < strlen(command); i++)
@@ -116,7 +123,7 @@ int pars(FILE *fp, int debag) {
     com[i - curi] = '\0';
     x = atoi(com);
     int ans = sw(numbcom);
-    fprintf(out, "Ac: %2.3f R: %2.3f \n", Ac, R);
+    fprintf(out, "Ac: %2.3f R: %i x: %i\n", Ac, R, x);
     fclose(out);
     out = fopen("output.txt", "a");
     return ans;
@@ -158,52 +165,104 @@ int sw(char *numbcom) {
     }
 
     if (strcmp(numbcom, "R") == 0) {
-        R = 0;
-        R += x;
+        if (R < 0 || R > 1023) {
+            printf("Exception in code: Index of bound.");
+            return 10;
+        }
+        registerData[R] = 0;
+        registerData[R] += x;
         return 0;
     }
     if (strcmp(numbcom, "A") == 0) {
+        if (R < 0 || R > 1023) {
+            printf("Exception in code: Index of bound.");
+            return 10;
+        }
         Ac = 0;
-        Ac = R;
+        Ac = registerData[R];
         return 0;
     }
     if (strcmp(numbcom, "X") == 0) {
-        x = (int) (x * R);
+        if (R < 0 || R > 1023) {
+            printf("Exception in code: Index of bound.");
+            return 10;
+        }
+        x = (int) (x * registerData[R]);
         Ac = 0;
-        R = x;
+        registerData[R] = x;
         return 0;
     }
     if (strcmp(numbcom, "--") == 0) {
-        R = 0;
-        R = Ac / x;
+        if (R < 0 || R > 1023) {
+            printf("Exception in code: Index of bound.");
+            return 10;
+        }
+        registerData[R] = 0;
+        registerData[R] = Ac / x;
+        return 0;
+    }
+    if (strcmp(numbcom, "C") == 0) {
+        R--;
+        if (R < 0 || R > 1023) {
+            printf("Exception in code: Index of bound.");
+            return 10;
+        }
+        return 0;
+    }
+    if (strcmp(numbcom, "C'") == 0) {
+        R++;
+        if (R < 0 || R > 1023) {
+            printf("Exception in code: Index of bound.");
+            return 10;
+        }
         return 0;
     }
     if (strcmp(numbcom, "Cc") == 0) {
-        x = ~x;
+        if (Ac > 0) R = x - 1;
         return 0;
     }
     if (strcmp(numbcom, "Cc'") == 0) {
+        if (Ac > 0) R = x + 1;
+        return 0;
+    }
+    if (strcmp(numbcom, "S") == 0) {
+        registerData[x] = Ac;
+        R = x;
+        if (R < 0 || R > 1023) {
+            printf("Exception in code: Index of bound.");
+            return 10;
+        }
         return 0;
     }
     if (strcmp(numbcom, "Sp") == 0) {
-        Ac = Ac * 2;
+        if (R < 11 || R > 1023 || x < 11 || x > 1023) {
+            printf("Exception in code: Index of bound.");
+            return 10;
+        }
+        for (i = 0; i < 12; i++) {
+            registerData[R - 11 + i] = registerData[x - 11 + i];
+        }
         return 0;
     }
     if (strcmp(numbcom, "Sp'") == 0) {
-        Ac = Ac * 2;
+        if (R < 0 || R > 1012 || x < 11 || x > 1023) {
+            printf("Exception in code: Index of bound.");
+            return 10;
+        }
+        for (i = 0; i < 12; i++) {
+            registerData[R + i] = registerData[x - 11 + i];
+        }
         return 0;
     }
     if (strcmp(numbcom, "L") == 0) {
-
         Ac = Ac * 2;
         return 0;
     }
     if (strcmp(numbcom, "R") == 0) {
-
         Ac = Ac / 2;
         return 0;
     } else {
-        printf("  --Exception: Unsupported command.");
+        printf("Exception in code: Unsupported command.");
         return 666;
     }
 }
