9988035be8d936fc30e630e44ac5361414fa3e44
Konstantin <kazachenko.1998k@gmail.com>
2018-06-01 02:00:35 +0300

    избавился от goto

--- main.c
+++ main.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <conio.h>
 #include <stdbool.h>
+#include <conio.h>
 #include "constant.h"
 #include "help_func.h"
 
@@ -19,40 +19,45 @@ int run_code_step_by_step_with_printing_results();
 int key;
 char answer[255];
 
+
 int main() {
-    stop:;
-    initialization();
-    printf("\n Choose way of running program: \n"
-           " fr       Full running \n"
-           " rfors    Running count of steps \n"
-           " sbs      Run code step by step \n"
-           " sbs -deb Run code step by step with printing results \n"
-           " exit     exit from program\n");
-    enter:;
-    gets(answer);                                   //fgets - это чтение из файла а не из с консоли)))
-    if (strcmp(answer, "fr") == 0) {                //полное выполнение кода
-        if (full_running() != 0) goto stop;
-    } else if (strcmp(answer, "rfors") == 0) {      //выполнение определенного числа шагов
-        if (running_count_of_steps() != 0) goto stop;
-    } else if (strcmp(answer, "sbs") == 0) {        //выполнить код пошагово
-        if (run_code_step_by_step() != 0) goto stop;
-    } else if (strcmp(answer, "sbs -deb") == 0) {   //выполнить код пошагово с выводом результата
-        if (run_code_step_by_step_with_printing_results() != 0) goto stop;
-    } else if (strcmp(answer, "exit") == 0) {
-        return 0;
-    } else if (strcmp(answer, "help") == 0 || strcmp(answer, "h") == 0) {
-        goto stop;
-    } else if (strcmp(answer, "") == 0) {
-        goto enter;
-    } else {
-        printf("Not found command\n");
-        goto stop;
+    int runing_state = 1;
+    while (runing_state == 1 || runing_state == 3) {
+
+        if (runing_state != 3) {
+            initialization();
+            printf("\n Choose way of running program: \n"
+
+                   " fr       Full running \n"
+                   " rfors    Running count of steps \n"
+                   " sbs      Run code step by step \n"
+                   " sbs -deb Run code step by step with printing results \n"
+                   " exit     exit from program\n");
+        }
+        gets(answer);                             //fgets - это чтение из файла а не из с консоли)))  но чем это заменить я так и не придумал, хотя можно использовать алфавит вмсето клавишь но так не интересно
+        if (strcmp(answer, "fr") == 0) {                //полное выполнение кода
+            if (full_running() != 0) runing_state = 2;
+        } else if (strcmp(answer, "rfors") == 0) {      //выполнение определенного числа шагов
+            if (running_count_of_steps() != 0) runing_state = 2;
+        } else if (strcmp(answer, "sbs") == 0) {        //выполнить код пошагово
+            if (run_code_step_by_step() != 0) runing_state = 2;
+        } else if (strcmp(answer, "sbs -deb") == 0) {   //выполнить код пошагово с выводом результата
+            if (run_code_step_by_step_with_printing_results() != 0) runing_state = 2;
+        } else if (strcmp(answer, "exit") == 0) {
+            return 0;
+        } else if (strcmp(answer, "help") == 0 || strcmp(answer, "h") == 0) {
+            runing_state = 2;
+        } else if (strcmp(answer, "") == 0) {
+            runing_state = 3;
+        } else {
+            printf("Not found command\n");
+            runing_state = 2;
+        }
+        fclose(fp);
+        fclose(out);
+        if (runing_state == 1) printf("You code completed successful. Press any button");
+        if (runing_state == 2) runing_state = 1;
     }
-    fclose(fp);
-    fclose(out);
-    printf("You code completed successful. Press any button");
-    getch();
-    goto stop;
 }
 
 int initialization() {
@@ -69,8 +74,7 @@ int initialization() {
 
 int running_count_of_steps() {
     printf("Input count of steps \n");
-    int countSteps = 0;
-    scanf("%d", &countSteps);
+    int countSteps = getch();
     while (countSteps > 0) {
         int result = pars(fp, 0);
         if (result != 0) return result;
